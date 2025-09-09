876ec1187221049e3ca8331f4592633cae4103cd
sharat mandala <mandalasharath@gmail.com>
2025-09-08 23:58:00 -0400

    updated port according to render deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,5 +27,5 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))  # Render expects $PORT (usually 10000)
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
